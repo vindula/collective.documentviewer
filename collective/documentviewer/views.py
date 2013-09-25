a0e4ee8ac6e79f1768c902fdc4b9c9caeddda573
Denis Raison <denisraison@liberiun.com>
2013-09-25 11:02:47 -0300

    Alterado o zoom padrão da visao do documento

--- collective/documentviewer/views.py
+++ collective/documentviewer/views.py
@@ -218,14 +218,16 @@ window.initializeDV = function(){
         sidebar: %(sidebar)s,
         width: %(width)s,
         search: %(search)s,
-        container: '#DV-container' });
+        container: '#DV-container',
+        zoom: %(zoom)s });
 }
 if(hash.search("\#(document|pages|text)\/") != -1 || (%(fullscreen)s &&
         hash != '#bypass-fullscreen')){
     window.currentDocument = DV.load(window.documentData, {
         sidebar: true,
         search: %(search)s,
-        container: document.body });
+        container: document.body,
+        zoom: %(zoom)s });
     jQuery('body').addClass('fullscreen');
 }else{
     window.initializeDV();
@@ -238,7 +240,8 @@ if(hash.search("\#(document|pages|text)\/") != -1 || (%(fullscreen)s &&
     'sidebar': str(sidebar).lower(),
     'search': str(search).lower(),
     'width': width,
-    'data': json.dumps(self.dv_data())
+    'data': json.dumps(self.dv_data()),
+    'zoom': '800'
 }
 
 
